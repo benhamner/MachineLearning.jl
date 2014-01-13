c339fe69f6a787f454f6356d38c18d0fa033c427
Ben Hamner <ben@benhamner.com>
2014-01-13 08:53:59 -0800

    functional basic neural net

--- src/neural_net.jl
+++ src/neural_net.jl
@@ -3,7 +3,7 @@ type NeuralNetOptions
 	hidden_layers::Vector{Int} # sizes of hidden layers
 	num_passes::Int
 
-	NeuralNetOptions() = new(true, [50], 100)
+	NeuralNetOptions() = new(true, [100], 200)
 end
 
 type NeuralNetLayer
@@ -29,6 +29,7 @@ function train(x::Array{Float64, 2}, y::Vector{Int}, opts::NeuralNetOptions)
 
 	net = initialize_net(opts, classes, num_features)
 	for i=1:opts.num_passes
+		println(i)
 		for j=1:num_samples
 			actual = zeros(num_classes)
 			actual[classes_map[y[j]]] = 1.0
@@ -69,13 +70,13 @@ function update_weights!(neural_net::NeuralNet, sample::Vector{Float64}, actual:
 	deltas = activations[length(activations)] - actual
 
 	for i=length(neural_net.layers):-1:1
-		deltas = neural_net.layers[i].weights'*deltas
-		deltas = deltas[2:length(deltas)]
-		deltas = deltas.*sigmoid_gradient(outputs[i])
-		gradient = update_size*deltas*activations[i]'
-		println(size(gradient))
-		println(size(neural_net.layers[i].weights))
-		neural_net.layers[i].weights += gradient
+		gradient = update_size*deltas*hcat(1,activations[i]')
+		if i>1
+			deltas = neural_net.layers[i].weights'*deltas
+			deltas = deltas[2:length(deltas)]
+			deltas = deltas.*sigmoid_gradient(outputs[i])
+		end
+		neural_net.layers[i].weights -= gradient
 	end
 end
 
@@ -99,3 +100,14 @@ xs = randn(10000, 5)
 ys = int(map(x->x>0.0, xs[:,1]-xs[:,2]+3*xs[:,3]+xs[:,4].*xs[:,5]))
 opts = NeuralNetOptions()
 net = train(xs, ys, opts)
+
+for i=1:10
+	println(i, " ", predict(net, vec(xs[i,:]))[2], " ", ys[i])
+end
+
+xts = randn(10, 5)
+yts = int(map(x->x>0.0, xts[:,1]-xts[:,2]+3*xts[:,3]+xts[:,4].*xts[:,5]))
+
+for i=1:10
+	println(i, " ", predict(net, vec(xts[i,:]))[2], " ", yts[i])
+end
