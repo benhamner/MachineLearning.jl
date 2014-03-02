fd08a4b96cfb663b7d0b0a622ce698cd12577cfb
Ben Hamner <ben@benhamner.com>
2014-03-01 22:55:39 -0800

    unit tests should pass now

--- test/common.jl
+++ test/common.jl
@@ -2,15 +2,15 @@ using Base.Test
 using MachineLearning
 using RDatasets
 
-iris = data("datasets", "iris")
+iris = dataset("datasets", "iris")
 colnames = names(iris)
-m_iris = float_matrix(iris[filter(x->x!="Species", colnames)])
+m_iris = float_matrix(iris[filter(x->x!=:Species, colnames)])
 @test m_iris[1,1]==5.1
 @test m_iris[150,4]==1.8
 
 train, test = split_train_test(iris)
-forest = fit(train, "Species", classification_forest_options(num_trees=10))
+forest = fit(train, :Species, classification_forest_options(num_trees=10))
 yhat = predict(forest, test)
-ytest = [x for x=test["Species"]]
+ytest = [x for x=test[:Species]]
 acc = accuracy(ytest, yhat)
 @test acc>0.8
