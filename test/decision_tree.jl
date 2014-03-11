e5f18f994553501e33cbca36a8afedabd06c2d85
Ben Hamner <ben@benhamner.com>
2014-03-10 22:05:56 -0700

    removed deprecated tests

--- test/decision_tree.jl
+++ test/decision_tree.jl
@@ -14,34 +14,6 @@ require("linear_data.jl")
 @test classification_split_location([1,2],2)==(0.0, 1)
 @test classification_split_location([2,1,1,1,2,2,2,2,2],2)==(3/18, 4)
 
-leaf1 = ClassificationLeaf([1.0,0.0])
-leaf2 = ClassificationLeaf([0.5,0.5])
-leaf3 = ClassificationLeaf([1.0,0.0])
-leaf4 = ClassificationLeaf([1.0,0.0])
-leaf5 = ClassificationLeaf([0.0,1.0,0.0])
-
-branch1 = DecisionBranch(1, 0.1, leaf1, leaf2)
-branch2 = DecisionBranch(1, 0.2, branch1, leaf3)
-branch3 = DecisionBranch(1, 0.3, leaf4, leaf5)
-branch4 = DecisionBranch(1, 0.4, branch2, branch3)
-
-tree = ClassificationTree(DecisionTree(branch4), [1,2], 1, ClassificationTreeOptions())
-
-@test length(leaf1)==1
-@test length(leaf2)==1
-@test length(branch1)==3
-@test length(branch2)==5
-@test length(branch3)==3
-@test length(branch4)==9
-@test length(tree)==9
-
-@test depth(leaf1)==1
-@test depth(branch1)==2
-@test depth(branch2)==3
-@test depth(branch3)==2
-@test depth(branch4)==4
-@test depth(tree)==4
-
 x=randn(2500, 2)
 y=int(map(i->x[i,1]>0 || x[i,2]>0, 1:size(x,1)))
 x_train, y_train, x_test, y_test = split_train_test(x, y)
