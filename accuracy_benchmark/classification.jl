1fcff7aff0bcb15cc0b17dbd1355a3b97e764377
Ben Hamner <ben@benhamner.com>
2014-03-22 22:48:42 -0700

    started adding classificaiton comparisons

---
+++ accuracy_benchmark/classification.jl
@@ -0,0 +1,66 @@
+using DataFrames
+using MachineLearning
+using RDatasets
+
+type ClassificationAccuracy
+    model_name::String
+    julia_options::ClassificationModelOptions
+    r_script_name::ASCIIString
+    python_script_name::ASCIIString
+end
+
+random_forest = ClassificationAccuracy("Random Forest", classification_forest_options(num_trees=100),   "",             "classification.py")
+decision_tree = ClassificationAccuracy("Decision Tree", classification_tree_options(),                  "",             "classification.py")
+
+datasets = [("datasets", "iris",   :Species, 0.5)]
+
+algorithms = [decision_tree,
+              random_forest]
+
+for algorithm=algorithms
+    println("ALGORITHM: ", algorithm.model_name)
+    for (pkg, dataset_name, colname, acc_threshold) = datasets
+        println(" - Dataset ", dataset_name)
+        data = dataset(pkg, dataset_name)
+        #columns = filter(x->eltype(data[x]) <: Number, names(data))
+        #data = data[columns]
+
+        train, test = split_train_test(data)
+        ytest = [x for x=test[colname]]
+        t0 = time()
+        model = fit(train, colname, algorithm.julia_options)
+        yhat = predict(model, test)
+        t1 = time()
+        acc = accuracy(ytest, yhat)
+        println(@sprintf("   - Julia Accuracy:  %0.3f\tElapsed Time: %0.2f", acc, t1-t0), "\t", algorithm.julia_options)
+
+        train = float_dataframe(train)
+        test  = float_dataframe(test)
+
+        train[:is_test] = "false"
+        test[:is_test]  = "true"
+        data_file    = tempname()
+        results_file = tempname()
+
+        writetable(data_file, rbind(train, test))
+
+        if algorithm.r_script_name != ""
+            t0 = time()
+            run(Cmd(Union(UTF8String, ASCIIString)["Rscript", algorithm.r_script_name, results_file, data_file, string(colname), algorithm.model_name]))
+            t1 = time()
+            r_results, header = readcsv(results_file, Float64, has_header=true)
+            acc = accuracy(ytest, vec(r_results))
+            println(@sprintf("   - R Accuracy:      %0.3f\tElapsed Time: %0.2f", acc, t1-t0), "\t", algorithm.r_script_name)
+        end
+
+        results_file = tempname()
+        if algorithm.python_script_name != ""
+            t0 = time()
+            run(Cmd(Union(UTF8String, ASCIIString)["python", algorithm.python_script_name, results_file, data_file, string(colname), algorithm.model_name]))
+            t1 = time()
+            python_results, header = readcsv(results_file, Float64, has_header=true)
+            acc = accuracy(ytest, vec(python_results))
+            println(@sprintf("   - Python Accuracy: %0.3f\tElapsed Time: %0.2f", acc, t1-t0), "\t", algorithm.python_script_name)
+        end
+    end
+end
---
+++ accuracy_benchmark/classification.py
@@ -0,0 +1,36 @@
+import csv
+import pandas as pd
+import sys
+
+def main():
+    output_file = sys.argv[1]
+    input_file  = sys.argv[2]
+    target_name = sys.argv[3]
+    model_name  = sys.argv[4]
+
+    data  = pd.read_csv(input_file)
+    train = data[data["is_test"] == False]
+    test  = data[data["is_test"] == True]
+
+    x_train = train[[c for c in train.keys() if c != target_name and c != "is_test"]]
+    y_train = train[target_name]
+    x_test  = test [[c for c in test.keys()  if c != target_name and c != "is_test"]]
+
+    if model_name=="Random Forest":
+        from sklearn.ensemble import RandomForestClassifier
+        model = RandomForestRegressor(n_estimators=100)
+    elif model_name=="Decision Tree":
+        from sklearn.tree import DecisionTreeClassifier
+        model = DecisionTreeRegressor()
+
+    model.fit(x_train, y_train)
+    y_hat = model.predict(x_test)
+
+    f = open(output_file, "w")
+    f.write("result\n")
+    f.write("\n".join([str(x) for x in y_hat]))
+    f.write("\n")
+    f.close()
+
+if __name__=="__main__":
+    main()
