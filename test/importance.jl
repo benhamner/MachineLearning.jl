5a0d139f2fb86f7164f5e4e685478d4214bc6996
Ben Hamner <ben@benhamner.com>
2014-05-04 21:15:27 -0700

    fixed importance results test

--- test/importance.jl
+++ test/importance.jl
@@ -6,7 +6,8 @@ x = randn(1000,3)
 model = [10.0;1.0;0.0]
 y = Int[y>0.0?1:0 for y=x*model]
 
-importance = importances(x, y, classification_forest_options())
-@test importance[1]>importance[2]
-@test importance[2]>importance[3]
-@test importance[3]<0.01*importance[1]
+res = importances(x, y, classification_forest_options())
+
+@test res.importances[1]>res.importances[2]
+@test res.importances[2]>res.importances[3]
+@test res.importances[3]<0.01*res.importances[1]
