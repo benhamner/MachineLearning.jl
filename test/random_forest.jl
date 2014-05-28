7c58d85dc98c2f4d412e9c7f448467363b90b478
Ben Hamner <ben@benhamner.com>
2014-05-27 22:30:00 -0700

    random_forest -> evaluate api

--- test/random_forest.jl
+++ test/random_forest.jl
@@ -5,12 +5,8 @@ require("linear_data.jl")
 
 num_features=5
 x, y = linear_data(2500, num_features)
-x_train, y_train, x_test, y_test = split_train_test(x, y)
-
-forest = fit(x_train, y_train, classification_forest_options(num_trees=10, display=true))
-println(forest)
-yhat = predict(forest, x_test)
-acc = accuracy(y_test, yhat)
+split = split_train_test(x, y)
+acc = evaluate(split, classification_forest_options(num_trees=10, display=true), accuracy)
 println("Linear Accuracy: ", acc)
 @test acc>0.80
 
@@ -18,11 +14,7 @@ model = randn(10)
 model[3] = 100.0
 x     = randn(2500, 10)
 y     = x*model
-x_train, y_train, x_test, y_test = split_train_test(x, y)
-
-forest = fit(x_train, y_train, regression_forest_options())
-println(forest)
-yhat = predict(forest, x_test)
-correlation = cor(y_test, yhat)
+split = split_train_test(x, y)
+correlation = evaluate(split, regression_forest_options(), cor)
 println("Linear Pearson Correlation: ", correlation)
 @test correlation>0.80
