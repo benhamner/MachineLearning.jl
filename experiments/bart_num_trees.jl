bdba595e2d16e2423e168eb587f3f8929a030531
Ben Hamner <ben@benhamner.com>
2014-07-08 07:29:07 -0700

    added num_trees experiment

---
+++ experiments/bart_num_trees.jl
@@ -0,0 +1,20 @@
+using MachineLearning
+using RDatasets
+
+options = SupervisedModelOptions[
+               bart_options(num_trees=10,   num_draws=1000),
+               bart_options(num_trees=50,   num_draws=1000),
+               bart_options(num_trees=100,  num_draws=1000),
+               bart_options(num_trees=200,  num_draws=1000),
+               bart_options(num_trees=500,  num_draws=1000),
+               bart_options(num_trees=1000, num_draws=1000),]
+
+data = dataset("car", "Prestige")
+data_generator(seed) = split_train_test(data, :Prestige, seed=seed)
+
+res = compare(data_generator, options, ["10 Trees", "50 Trees", "100 Trees", "200 Trees", "500 Trees", "1000 Trees"], cor)
+
+dist = by(res, :Name, df -> DataFrame(Mean=mean(df[:Score]),
+                                      Q1=quantile(df[:Score], 0.25),
+                                      Q3=quantile(df[:Score], 0.75)))
+println(dist)
