b68a4a7ff369c3100c272559c9323ab3ea2b06b0
Ben Hamner <ben@benhamner.com>
2014-07-17 23:43:28 -0700

    added plot

--- experiments/bart_num_trees.jl
+++ experiments/bart_num_trees.jl
@@ -14,6 +14,8 @@ data_generator(seed) = split_train_test(data, :Prestige, seed=seed)
 
 res = compare(data_generator, options, ["10 Trees", "50 Trees", "100 Trees", "200 Trees", "500 Trees", "1000 Trees"], cor)
 
+draw(PNG("bart_num_trees.png", 8inch, 6inch), plot(res, x=:Name, y=:Score, Geom.boxplot))
+
 dist = by(res, :Name, df -> DataFrame(Mean=mean(df[:Score]),
                                       Q1=quantile(df[:Score], 0.25),
                                       Q3=quantile(df[:Score], 0.75)))
