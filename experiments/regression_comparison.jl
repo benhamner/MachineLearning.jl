baf23935f17bce3c1fb4b5b0cb0e5fb627895d0a
Ben Hamner <ben@benhamner.com>
2014-07-20 10:16:47 -0700

    added BART to regression experiment

--- experiments/regression_comparison.jl
+++ experiments/regression_comparison.jl
@@ -4,12 +4,13 @@ using RDatasets
 
 options = SupervisedModelOptions[
                regression_tree_options(),
-               regression_forest_options()]
+               regression_forest_options(),
+               bart_options()]
 
 data = dataset("car", "Prestige")
 data_generator(seed) = split_train_test(data, :Prestige, seed=seed)
 
-res = compare(data_generator, options, ["Tree", "Forest"], cor)
+res = compare(data_generator, options, ["Tree", "Forest", "BART"], cor)
 
 draw(PNG("regression_comparison.png", 8inch, 6inch), plot(res, x=:Name, y=:Score, Geom.boxplot))
 
