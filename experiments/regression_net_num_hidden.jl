ae27c906c521d1571bc6f434a9d9375d214d387f
Ben Hamner <ben@benhamner.com>
2014-07-28 08:21:00 -0700

    updated exp

--- experiments/regression_net_num_hidden.jl
+++ experiments/regression_net_num_hidden.jl
@@ -2,11 +2,11 @@ using Gadfly
 using MachineLearning
 using RDatasets
 
-opts_generator(num_hidden::Int) = regression_net_options(hidden_layers=[num_hidden], stop_criteria=StopAfterIteration(1000), learning_rate=1.0)
+opts_generator(num_hidden::Int) = RegressionPipelineOptions(TransformerOptions[ZmuvOptions()], regression_net_options(hidden_layers=[num_hidden]))
 opts_sweep = [10:10:100]
 
-data = dataset("car", "Prestige")
-data_generator(seed) = split_train_test(data, :Prestige, seed=seed)
+data = dataset("datasets", "quakes")
+data_generator(seed) = split_train_test(data, :Mag, seed=seed)
 
 res = compare(data_generator, opts_generator, opts_sweep, cor)
 draw(PNG("regression_net_num_hidden.png", 8inch, 6inch), plot(res, x=:Name, y=:Score))
