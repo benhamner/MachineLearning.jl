cf6fbc70e5fc73032c68ebb770c6c246f418a19e
Ben Hamner <ben@benhamner.com>
2014-07-28 07:54:43 -0700

    added unit tests

--- test/pipeline.jl
+++ test/pipeline.jl
@@ -17,3 +17,20 @@ opts = ClassificationPipelineOptions(TransformerOptions[ZmuvOptions()], net_opts
 acc = evaluate(split, opts, accuracy)
 println("Linear Accuracy, Normalized: ", acc)
 @test acc>0.80
+
+x = randn(1000, 5)
+m = [100.0, 10.0, -1.0, 0.1, 0.01]
+y = x*m + randn()
+
+split = split_train_test(0.0001*x, y)
+
+net_opts = regression_net_options()
+opts = RegressionPipelineOptions(TransformerOptions[], net_opts)
+score = evaluate(split, opts, cor)
+println("Linear Accuracy, Unnormalized: ", score)
+@test score<0.55
+
+opts = RegressionPipelineOptions(TransformerOptions[ZmuvOptions()], net_opts)
+score = evaluate(split, opts, cor)
+println("Linear Accuracy, Normalized: ", score)
+@test score>0.80
